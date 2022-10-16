8cffc88699491e0de8ae41b92a879880dfd224d9
Jenil Ghevariya <76035914+jenilghevariya09@users.noreply.github.com>
2022-10-16 22:21:19 +0530

    Add files via upload

---
+++ Mobile-Axios-1-master/src/assets/theme-dark/components/dialog/dialogTitle.js
@@ -0,0 +1,18 @@
+// FMP Technologies React base styles
+import typography from "assets/theme-dark/base/typography";
+
+// FMP Technologies React helper functions
+import pxToRem from "assets/theme-dark/functions/pxToRem";
+
+const { size } = typography;
+
+const dialogTitle = {
+  styleOverrides: {
+    root: {
+      padding: pxToRem(16),
+      fontSize: size.xl,
+    },
+  },
+};
+
+export default dialogTitle;
